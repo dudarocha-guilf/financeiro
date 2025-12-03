e56e722369a801ec6d72eed195406e7cd1c56629
dudarocha-guilf <maria.e.rocha23@aluno.senai.br>
2025-12-03 16:46:57 -0300

    divei 03/12

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// configuração do bd
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'financeiro');
+define('DB_USER', 'root');
+define('DB_PASS', 'mysql');
+
+// conexão com o bd
+try {
+    $conn = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT."; dbname=".DB_NAME.";charset=utf8mb4", DB_USER, DB_PASS); $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("ERRO na conexão com o banco de dados: " . $e ->getMessage());
+}
+?>
